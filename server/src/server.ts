3062836714448612b8218aaa3ffe637ed4da355c
Mattias Åslund <mattias.aslund@shoppa.com>
2024-01-27 14:01:45 +0100

    Language server can initialize, but does not do anything useful

---
+++ server/src/server.ts
@@ -0,0 +1,62 @@
+import log from "./log";
+import { initialize } from "./methods/initialize";
+
+interface Message {
+	jsonrpc: string;
+}
+
+export interface RequestMessage extends Message {
+	id: number | string;
+	method: string;
+	params?: unknown[] | object;
+}
+
+// interface NotificationMessage extends Message {
+// 	method: string;
+// 	params?: unknown[] | object;
+// }
+
+type RequestMethod = (message: RequestMessage) => unknown;
+const methodLookup: Record<string, RequestMethod> = { initialize };
+
+const respond = (id: RequestMessage["id"], result: unknown) => {    
+    const message = JSON.stringify({id, result});
+    const messageLength = Buffer.byteLength(message, "utf8");
+    const header = `Content-Length: ${messageLength}\r\n\r\n`;
+
+    log.write(header + message);
+    process.stdout.write(header + message);
+};
+
+let buffer = "";
+
+process.stdin.on("data", (chunk) => {
+    buffer += chunk.toString();
+
+    while(true) {
+        const contentLengthMatch = /Content-Length: (\d+)/.exec(buffer);
+
+        if (!contentLengthMatch) {
+            break;
+        }
+
+        const contentLength = Number.parseInt(contentLengthMatch[1], 10);
+        const messageStart = buffer.indexOf("\r\n\r\n") + 4;
+
+        if (buffer.length < messageStart + contentLength) {
+            break;
+        }
+
+        const rawMessage = buffer.slice(messageStart, messageStart + contentLength);
+        const message = JSON.parse(rawMessage);
+        log.write({id: message.id, method: message.method});
+
+        buffer = buffer.slice(messageStart + contentLength);
+
+
+        const method = methodLookup[message.method];
+        if(method) {
+            respond(message.id, method(message));
+        }
+    }
+});
